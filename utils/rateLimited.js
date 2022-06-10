b05bf7cecb6363db1e2caf053e92a2f216784069
skye5000 <skylykat69@gmail.com>
2022-06-10 14:05:58 +0300

    fix rate limited toast

--- utils/rateLimited.js
+++ utils/rateLimited.js
@@ -4,7 +4,7 @@ const t = new Set();
 
 module.exports = (ws, rl) => {
 	if (t.has(ws.uuid)) return;
-	send(ws, { type: 'toast', toast: 'error', top: rl ? rl : 'rate_limited', bottom: rl ? 'rate_limited' : '' });
+	send(ws, { type: 'toast', toast: 'error', top: 'rate_limited', bottom: rl ? rl : 'rate_limited' });
 	t.add(ws.uuid);
 	setTimeout(() => { t.delete(ws.uuid); }, 5000);
 }
