eff9053763ef1c6e2eb22f6be095e6fc7df87009
Skylar Stevens <skylykat69@gmail.com>
2022-06-28 18:01:48 +0300

    fix logger

--- index.js
+++ index.js
@@ -21,7 +21,16 @@ console.clear();
 
 const logger = winston.createLogger({
 	level: 'info',
-	format: winston.format.json()
+	format: winston.format.json(),
+	transports: [
+		new winston.transports.Console({
+			level: 'info',
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple()
+			)
+		})
+	]
 });
 
 (async () => {
