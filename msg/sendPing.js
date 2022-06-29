2c2f3809bab63a536965dac6e8f2db589f093473
Skylar Stevens <skylykat69@gmail.com>
2022-06-29 21:47:00 +0300

    fix pingSize

--- msg/sendPing.js
+++ msg/sendPing.js
@@ -3,9 +3,9 @@ const utils = require('../utils');
 module.exports = async (wss, ws, msg, events, mongo, redis, clients, logger) => {
 	if (!ws.ready) return;
 
-	if (!msg.data || !msg.name) return ws.close();
+	if (!msg.data.isArray() || !msg.name) return ws.close();
 
-	ws.limits.pingSize.consume(ws.ip, msg.data.length).then(async () => {
+	ws.limits.pingSize.consume(ws.ip, new Blob([JSON.stringify(msg.data)]).size /* might cause memory leaks, will fix if it happens */).then(async () => {
 		ws.limits.pingRate.consume(ws.ip, 1).then(async () => {
 			events.emit(ws.uuid, { type: 'ping', data: msg.data, name: msg.name });
 		}).catch(() => { return utils.rateLimited(ws, 'ping_rate') });
