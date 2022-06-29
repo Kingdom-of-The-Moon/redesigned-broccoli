8cce1696f7d852ff4e50262753748036802aee7e
Skylar Stevens <skylykat69@gmail.com>
2022-06-29 21:42:05 +0300

    add name to ping

--- msg/sendPing.js
+++ msg/sendPing.js
@@ -3,11 +3,11 @@ const utils = require('../utils');
 module.exports = async (wss, ws, msg, events, mongo, redis, clients, logger) => {
 	if (!ws.ready) return;
 
-	if (!msg.data) return ws.close();
+	if (!msg.data || !msg.name) return ws.close();
 
 	ws.limits.pingSize.consume(ws.ip, msg.data.length).then(async () => {
 		ws.limits.pingRate.consume(ws.ip, 1).then(async () => {
-			events.emit(ws.uuid, { type: 'ping', data: msg.data });
+			events.emit(ws.uuid, { type: 'ping', data: msg.data, name: msg.name });
 		}).catch(() => { return utils.rateLimited(ws, 'ping_rate') });
 	}).catch(() => { return utils.rateLimited(ws, 'ping_size') });
 }
